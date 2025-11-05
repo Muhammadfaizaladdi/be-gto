a38e4fa5f4457931dbbb2b5311f37cf60ccb790b
Faizal Addi <faizal.addi@gmail.com>
2025-11-06 05:52:03 +0900

    feat: register api, validation, save to mongodb, and encryption

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import router from './routes/api'
+import  connectDb  from './utils/database'
 
+async function init(){
+    try {
+        
+        const result = await connectDb()
 
-const app = express()
+        console.log("database status:", result)
 
-const PORT = 3000
+        const app = express()
+        app.use(bodyParser.json())
 
-app.use('/api', router)
+        const PORT = 3000
+        app.use('/api', router)
 
-app.listen(PORT, ()=> {
-    console.log(`server is running on http://localhost:${PORT}`)
-});
+        app.listen(PORT, ()=> {
+            console.log(`server is running on http://localhost:${PORT}`)
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+init()
+
